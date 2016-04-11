NamelessRom/android_device_samsung_smdk4412-common

AdvancedDisplay: add gradle support, update Theme
 * also cleanup a little bit

Change-Id: I9d77d569574a380abe60a2b98f945ba454bc6243
Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

AdvancedDisplay/src/com/cyanogenmod/settings/device/mDNIeMode.java
@@ -25,6 +25,8 @@ import android.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
 
+import org.namelessrom.settings.device.R;
+
 public class mDNIeMode extends ListPreference implements OnPreferenceChangeListener {
 
     private static String FILE = null;

AdvancedDisplay/src/com/cyanogenmod/settings/device/mDNIeNegative.java
@@ -16,7 +16,6 @@
 
 package com.cyanogenmod.settings.device;
 
-import java.io.IOException;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.content.SharedPreferences;
@@ -24,6 +23,8 @@ import android.preference.Preference;
 import android.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
+
+import org.namelessrom.settings.device.R;
 
 public class mDNIeNegative extends ListPreference implements OnPreferenceChangeListener {
 

AdvancedDisplay/src/com/cyanogenmod/settings/device/mDNIeScenario.java
@@ -25,6 +25,8 @@ import android.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
 
+import org.namelessrom.settings.device.R;
+
 public class mDNIeScenario extends ListPreference implements OnPreferenceChangeListener {
 
     private static String FILE = null;

Haxynox/device_samsung_smdk4412-common

AdvancedDisplay/src/com/cyanogenmod/settings/device/mDNIeMode.java
@@ -25,6 +25,8 @@ import android.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
 
+import com.cyanogenmod.settings.device.R;
+
 public class mDNIeMode extends ListPreference implements OnPreferenceChangeListener {
 
     private static String FILE = null;

AdvancedDisplay/src/com/cyanogenmod/settings/device/mDNIeNegative.java
@@ -16,7 +16,6 @@
 
 package com.cyanogenmod.settings.device;
 
-import java.io.IOException;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.content.SharedPreferences;
@@ -24,6 +23,8 @@ import android.preference.Preference;
 import android.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
+
+import com.cyanogenmod.settings.device.R;
 
 public class mDNIeNegative extends ListPreference implements OnPreferenceChangeListener {
 

AdvancedDisplay/src/com/cyanogenmod/settings/device/mDNIeScenario.java
@@ -25,6 +25,8 @@ import android.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
 
+import com.cyanogenmod.settings.device.R;
+
 public class mDNIeScenario extends ListPreference implements OnPreferenceChangeListener {
 
     private static String FILE = null;