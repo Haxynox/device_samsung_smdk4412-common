NamelessRom/android_device_samsung_smdk4412-common

AdvancedDisplay: huge cleanup
Change-Id: I31fb8927cdb173187233af63d5c8fd6f74b8dba0
Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

AdvancedDisplay/src/com/cyanogenmod/settings/device/preferences/mDNIeMode.java
@@ -14,22 +14,28 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.cyanogenmod.settings.device.preferences;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
+import android.util.AttributeSet;
 
-import com.cyanogenmod.settings.device.preferences.mDNIeMode;
-import com.cyanogenmod.settings.device.preferences.mDNIeNegative;
-import com.cyanogenmod.settings.device.preferences.mDNIeScenario;
+import com.cyanogenmod.settings.device.DisplaySettings;
 
-public class Startup extends BroadcastReceiver {
+import org.namelessrom.settings.device.R;
+
+public class mDNIeMode extends mDNIeBasePreference {
 
     @Override
-    public void onReceive(final Context context, final Intent intent) {
-        mDNIeScenario.restore(context);
-        mDNIeMode.restore(context);
-        mDNIeNegative.restore(context);
+    public int getFileStringResId() {
+        return R.string.mdnie_mode_sysfs_file;
     }
+
+    public mDNIeMode(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public static void restore(Context context) {
+        mDNIeBasePreference.restore(context, DisplaySettings.KEY_MDNIE_MODE, R.string.mdnie_mode_sysfs_file);
+    }
+
 }

AdvancedDisplay/src/com/cyanogenmod/settings/device/preferences/mDNIeNegative.java
@@ -14,22 +14,28 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.cyanogenmod.settings.device.preferences;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
+import android.util.AttributeSet;
 
-import com.cyanogenmod.settings.device.preferences.mDNIeMode;
-import com.cyanogenmod.settings.device.preferences.mDNIeNegative;
-import com.cyanogenmod.settings.device.preferences.mDNIeScenario;
+import com.cyanogenmod.settings.device.DisplaySettings;
 
-public class Startup extends BroadcastReceiver {
+import org.namelessrom.settings.device.R;
+
+public class mDNIeNegative extends mDNIeBasePreference {
 
     @Override
-    public void onReceive(final Context context, final Intent intent) {
-        mDNIeScenario.restore(context);
-        mDNIeMode.restore(context);
-        mDNIeNegative.restore(context);
+    public int getFileStringResId() {
+        return R.string.mdnie_negative_sysfs_file;
     }
+
+    public mDNIeNegative(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public static void restore(Context context) {
+        mDNIeBasePreference.restore(context, DisplaySettings.KEY_MDNIE_NEGATIVE, R.string.mdnie_negative_sysfs_file);
+    }
+
 }

AdvancedDisplay/src/com/cyanogenmod/settings/device/preferences/mDNIeScenario.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device.preferences;
+
+import android.content.Context;
+
+import android.content.SharedPreferences;
+import android.util.AttributeSet;
+import android.preference.Preference;
+import android.preference.ListPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceManager;
+
+import com.cyanogenmod.settings.device.DisplaySettings;
+import com.cyanogenmod.settings.device.Utils;
+
+import org.namelessrom.settings.device.R;
+
+public class mDNIeScenario extends mDNIeBasePreference {
+
+    @Override
+    public int getFileStringResId() {
+        return R.string.mdnie_scenario_sysfs_file;
+    }
+
+    public mDNIeScenario(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public static void restore(Context context) {
+        mDNIeBasePreference.restore(context, DisplaySettings.KEY_MDNIE_SCENARIO, R.string.mdnie_scenario_sysfs_file);
+    }
+
+}

Haxynox/device_samsung_smdk4412-common

AdvancedDisplay/src/com/cyanogenmod/settings/device/preferences/mDNIeMode.java
@@ -14,22 +14,28 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.cyanogenmod.settings.device.preferences;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
+import android.util.AttributeSet;
 
-import com.cyanogenmod.settings.device.preferences.mDNIeMode;
-import com.cyanogenmod.settings.device.preferences.mDNIeNegative;
-import com.cyanogenmod.settings.device.preferences.mDNIeScenario;
+import com.cyanogenmod.settings.device.DisplaySettings;
 
-public class Startup extends BroadcastReceiver {
+import com.cyanogenmod.settings.device.R;
+
+public class mDNIeMode extends mDNIeBasePreference {
 
     @Override
-    public void onReceive(final Context context, final Intent intent) {
-        mDNIeScenario.restore(context);
-        mDNIeMode.restore(context);
-        mDNIeNegative.restore(context);
+    public int getFileStringResId() {
+        return R.string.mdnie_mode_sysfs_file;
     }
+
+    public mDNIeMode(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public static void restore(Context context) {
+        mDNIeBasePreference.restore(context, DisplaySettings.KEY_MDNIE_MODE, R.string.mdnie_mode_sysfs_file);
+    }
+
 }

AdvancedDisplay/src/com/cyanogenmod/settings/device/preferences/mDNIeNegative.java
@@ -14,22 +14,28 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.cyanogenmod.settings.device.preferences;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
+import android.util.AttributeSet;
 
-import com.cyanogenmod.settings.device.preferences.mDNIeMode;
-import com.cyanogenmod.settings.device.preferences.mDNIeNegative;
-import com.cyanogenmod.settings.device.preferences.mDNIeScenario;
+import com.cyanogenmod.settings.device.DisplaySettings;
 
-public class Startup extends BroadcastReceiver {
+import com.cyanogenmod.settings.device.R;
+
+public class mDNIeNegative extends mDNIeBasePreference {
 
     @Override
-    public void onReceive(final Context context, final Intent intent) {
-        mDNIeScenario.restore(context);
-        mDNIeMode.restore(context);
-        mDNIeNegative.restore(context);
+    public int getFileStringResId() {
+        return R.string.mdnie_negative_sysfs_file;
     }
+
+    public mDNIeNegative(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public static void restore(Context context) {
+        mDNIeBasePreference.restore(context, DisplaySettings.KEY_MDNIE_NEGATIVE, R.string.mdnie_negative_sysfs_file);
+    }
+
 }

AdvancedDisplay/src/com/cyanogenmod/settings/device/preferences/mDNIeScenario.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device.preferences;
+
+import android.content.Context;
+
+import android.content.SharedPreferences;
+import android.util.AttributeSet;
+import android.preference.Preference;
+import android.preference.ListPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceManager;
+
+import com.cyanogenmod.settings.device.DisplaySettings;
+import com.cyanogenmod.settings.device.Utils;
+
+import com.cyanogenmod.settings.device.R;
+
+public class mDNIeScenario extends mDNIeBasePreference {
+
+    @Override
+    public int getFileStringResId() {
+        return R.string.mdnie_scenario_sysfs_file;
+    }
+
+    public mDNIeScenario(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public static void restore(Context context) {
+        mDNIeBasePreference.restore(context, DisplaySettings.KEY_MDNIE_SCENARIO, R.string.mdnie_scenario_sysfs_file);
+    }
+
+}